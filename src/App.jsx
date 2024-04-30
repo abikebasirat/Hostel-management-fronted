10f5fe0a7c01ae50f567939146e2013e26106586
basirat10 <basiratamusat10@gmail.com>
2024-04-30 18:03:14 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { Route, Routes} from "react-router-dom";
+import AdminReg from "./Component/Register/AdminReg";
+import Login from "./Component/Register/Login";
+import StudentReg from "./Component/Register/StudentReg";
+import HomeDash from "./Component/Dashboard/HomeDash";
+import Layout from "./Component/Layout/Layout";
+import StudentDashboard from "./Component/Dashboard/StudentDashboard";
+import Rooms from "./Component/Dashboard/Rooms";
+import AdminPreview from "./Component/AdminPreview/AdminPreview";
+import Adttendance from "./Component/Adttendance/Adttendance";
+import Loader from "./Component/Loader/Loader";
+import { useState, useEffect } from "react";
+
+
+
+function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    },3000)
+  }, [])
+
+    const renderRoute = () => (
+  
+      <div>
+        <Routes>
+          <Route path="/" element={<AdminReg />} />
+
+          <Route path="/login" element={<Login />} />
+          
+          <Route path="/student-reg" element={
+          <Layout>
+          <StudentReg /> 
+          </Layout>
+          }/>
+          <Route
+            path="/homedash"
+            element={
+              <Layout>
+                <HomeDash /> 
+             </Layout>
+            }
+          />
+          <Route path="/studentdash" element={<StudentDashboard />}/>
+
+          <Route path="/room" element={<Rooms />}/>
+
+          <Route path="/adminsPrev" element={<AdminPreview />}/>
+         <Route path="adttendance" element={
+
+         <Layout>
+         < Adttendance/>
+          </Layout>
+        }/>
+        
+        </Routes>
+        {/* </Layout> */}
+      </div>
+    
+  );
+  return <>{loading ? <Loader/> : renderRoute()}</>
+}
+
+export default App;
---
+++ src/Component/Dashboard/Rooms.jsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import RoomTable from "./RoomTable";
+import Sidebar from "./Sidebar";
+
+
+
+const initialRooms = [
+  {
+    roomNumber: "101",
+    capacity: 4,
+    occupancy: 2,
+    status: "Available",
+    location: "Lakeside Manor, Riverside",
+  },
+  {
+    roomNumber: "102",
+    capacity: 3,
+    occupancy: 3,
+    status: "Occupied",
+    location: "Hillview Hostel, Springfield",
+  },
+  {
+    roomNumber: "103",
+    capacity: 4,
+    occupancy: 3,
+    status: "Available",
+    location: "Maplewood Lodge, Greenfield",
+  },
+];
+
+const Rooms = () => {
+  const [searchTerm, setSearchTerm] = useState(""); // Input Handler
+  const [rooms, setRooms] = useState(initialRooms); // Our data state
+  const [filteredData, setFilteredData] = useState(initialRooms); // Filter your data according to our input
+
+  const handleSearchChange = (e) => {
+    const term = e.target.value.toLowerCase();
+    setSearchTerm(term);
+    const filtered = rooms.filter(
+      (room) =>
+        room.roomNumber.toLowerCase().includes(term) ||
+        room.status.toLowerCase().includes(term) ||
+        room.location.toLowerCase().includes(term)
+    );
+    setFilteredData(filtered);
+  };
+  const handleAddRoom = (newRoomData) => {
+
+    setRooms([...rooms, newRoomData]);
+    setFilteredData([...rooms, newRoomData])
+
+  };
+
+  const handleUpdateRoom = (roomNumber, newStatus) => {
+    const updatedRooms = rooms.map((room) =>
+      room.roomNumber === roomNumber ? { ...room, status: newStatus } : room
+    );
+    setRooms(updatedRooms);
+    setFilteredData(updatedRooms);
+  };
+
+  const handleDeleteRoom = (roomNumber) => {
+    const updatedRooms = rooms.filter((room) => room.roomNumber !== roomNumber);
+    setRooms(updatedRooms);
+    setFilteredData(updatedRooms);
+  };
+
+  return (
+    <div className="container --flex-start --gap" >
+      <Sidebar />
+      <div>
+        <h1>Hostel Room Listing</h1>
+        <input
+          placeholder="Search by room number, status, or location"
+          type="text"
+          className="search"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        <RoomTable
+          rooms={filteredData}
+          onAddRoom={handleAddRoom}
+          onUpdateRoom={handleUpdateRoom}
+          onDeleteRoom={handleDeleteRoom}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Rooms;
