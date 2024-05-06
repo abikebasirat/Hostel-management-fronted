0d29200cf1d8647218ebce40a95873768a355ec4
basirat10 <basiratamusat10@gmail.com>
2024-05-06 11:55:16 +0100

    update

--- src/Component/Register/AdminReg.jsx
+++ src/Component/Register/AdminReg.jsx
@@ -63,7 +63,7 @@ const AdminReg = () => {
           <button className="--btn">Create an account</button>
         </form>
         <p>
-          Already have an account? <Link to="/login">Login</Link>
+          Already have an account? <Link to="/login">Login</Link>  || <Link to="/homedash">Go Home</Link>
         </p>
       </div>
     </div>
